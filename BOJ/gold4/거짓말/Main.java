a92dc6fcdd2e05f883f3a4051364423f60d75348
MonoDV <js727r@gmail.com>
2022-06-07 21:40:02 +0900

    gold4 거짓말

---
+++ BOJ/gold4/거짓말/Main.java
@@ -0,0 +1,22 @@
+package gold4.거짓말;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N,M;
+	static int T;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		T = Integer.parseInt(st.nextToken());
+		if (T > 0) {
+			
+		}
+	}
+
+}
